e1d0208b1cd6a18c689156e375a6eb4d4106656a
dansam <33277725+kwx4957@users.noreply.github.com>
2022-07-26 18:37:59 +0900

    StreamApi 7번 ~ 15번 완료

--- src/test/java/space/gavinklfong/demo/streamapi/StreamApiMine.java
+++ src/test/java/space/gavinklfong/demo/streamapi/StreamApiMine.java
@@ -4,15 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import space.gavinklfong.demo.streamapi.models.Customer;
 import space.gavinklfong.demo.streamapi.models.Order;
 import space.gavinklfong.demo.streamapi.models.Product;
 import space.gavinklfong.demo.streamapi.repos.CustomerRepo;
 import space.gavinklfong.demo.streamapi.repos.OrderRepo;
 import space.gavinklfong.demo.streamapi.repos.ProductRepo;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.*;
 import java.util.stream.Collectors;
 
 // https://blog.devgenius.io/15-practical-exercises-help-you-master-java-stream-api-3f9c86b1cf82
@@ -65,8 +65,17 @@ public class StreamApiMine {
     }
 
     //문제 4번
-    public void getProductByCustomerTier2Btw01F2021and01Ap2021(){
-//        List<Product> productList = orderRepo.findAll();
+    @Test
+    public void getProductsByCustomerT2btwFebtoApr(){
+        List<Product> re = orderRepo.findAll().stream()
+                .filter(order -> order.getCustomer().getTier()==2)
+                .filter(order -> order.getOrderDate().isAfter(LocalDate.of(2021,2,1)))
+                .filter(order -> order.getOrderDate().isBefore(LocalDate.of(2021,4,1)))
+                .flatMap(order -> order.getProducts().stream())
+                .distinct()
+                .collect(Collectors.toList());
+
+        re.forEach(z->log.info(z.toString()));
     }
 
     //problem 5
@@ -77,6 +86,7 @@ public class StreamApiMine {
                 .min(Comparator.comparing(Product::getPrice));
         log.info(cheapestProduct.toString());
     }
+
     //problem 6
     @Test
     public void getThreeMostRecentPlacedOrder(){
@@ -88,19 +98,120 @@ public class StreamApiMine {
 
         result.forEach(order -> log.info(order.toString()));
     }
+
     //problem 7
-//    @Test
-//    public void getOrder(){
-//        List<Order> result = orderRepo.findAll()
-//                .stream()
-//                .filter(order -> order.getOrderDate().isEqual(new LocalDAtetime(03,15,2021))
-//    }
+    @Test
+    public void getOrder(){
+        List<Product> result = orderRepo.findAll()
+                .stream()
+                .filter(order -> order.getOrderDate().isEqual(LocalDate.parse("2021-03-15")))
+                .peek(order -> log.info(order.toString()))
+                .flatMap(order -> order.getProducts().stream())
+                .collect(Collectors.toList());
+
+        result.forEach(product -> log.info(product.toString()));
+
+    }
+
     //problem 8
+    @Test
+    public void getOrderCalculateTotalLumpSum(){
+        Double result = orderRepo.findAll()
+                .stream()
+                .filter(order -> order.getOrderDate().isAfter(LocalDate.of(2021,2,1)))
+                .filter(order -> order.getOrderDate().isBefore(LocalDate.of(2021,3,1)))
+                .flatMap(order -> order.getProducts().stream())
+                .mapToDouble(Product::getPrice)
+                .sum();
+        System.out.println(result);
+    }
+
     //problem 9
+    @Test
+    public void getAvgPrice(){
+        Double result = orderRepo.findAll()
+                .stream()
+                .filter(order -> order.getOrderDate().isEqual(LocalDate.of(2021,3,15)))
+                .flatMap(order -> order.getProducts().stream())
+                .mapToDouble(Product::getPrice)
+                .average().getAsDouble();
+
+        System.out.println(result);
+
+    }
+
     //problem 10
+    @Test
+    public void get(){
+        DoubleSummaryStatistics result = productRepo.findAll().stream()
+                .filter(product -> product.getCategory().equalsIgnoreCase("Books"))
+                .mapToDouble(Product::getPrice)
+                .summaryStatistics();
+
+        System.out.printf("count = %1$d, average = %2$f, max = %3$f, min = %4$f, sum = %5$f%n",
+                result.getCount(), result.getAverage(), result.getMax(), result.getMin(), result.getSum());
+    }
+
     //problem 11
+    @Test
+    public void getMapOredrIdandProductCount(){
+        Map<Long,Integer> re = orderRepo.findAll()
+                .stream()
+                .collect(
+                        Collectors.toMap(Order::getId,
+                                order -> order.getProducts().size()));
+
+        System.out.println(re.toString());
+    }
+
     //problem 12
+    @Test
+    public void getOrderGroupByCustomer(){
+        Map<Customer,List<Order>> re = orderRepo.findAll()
+                .stream()
+                .collect(Collectors.groupingBy(Order::getCustomer));
+
+        System.out.println(re.toString());
+    }
+
     //problem 13
+    @Test
+    public void getMapOrderRecordAndProductTotleSum(){
+        Map<Object,Double> re = orderRepo.findAll()
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                z->z, order -> order.getProducts().stream()
+                                        .mapToDouble(Product::getPrice)
+                                        .sum())
+                );
+
+        System.out.println(re.toString());
+    }
+
     //problem 14
+    @Test
+    public void getListProductNameByCategory(){
+        Map<String,List<String>> re= productRepo.findAll()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Product::getCategory,
+                                Collectors.mapping(Product::getName, Collectors.toList())));
+
+        System.out.println(re.toString());
+    }
+
     //problem 15
+    @Test
+    public void getMostExpensiveProductByCategory(){
+        Map<String, Optional<Product>> result = productRepo.findAll()
+                .stream()
+                .collect(
+                        Collectors.groupingBy(
+                                Product::getCategory,
+                                Collectors.maxBy(
+                                        Comparator.comparingDouble(Product::getPrice))));
+        System.out.println(result.toString());
+    }
 }
